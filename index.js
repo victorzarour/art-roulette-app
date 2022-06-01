a1c2b7da9f962d43bcc2e6e99cd1281a5bc5cc62
Lori Henderson <lorim95@gmail.com>
2022-05-31 22:02:31 -0400

    Added event listener for subscribe button and added id for subscribe button

--- index.js
+++ index.js
@@ -64,3 +64,9 @@ function artWork(work){
 card.addEventListener("click", e => {
     document.querySelector(".row").style.display = "none"    
 })
+
+// Lori added the lines 69 - 72
+const btnSub = document.querySelector('button#btn-subscribe');
+const clickSub = document.addEventListener("click", function(btnSub){
+    alert("If you need more beautiful, historical works in your life, please consider subscribing for more.  Thank you!"); 
+});
